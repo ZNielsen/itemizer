17374429ad75c586a7e370bd06dc734b9d86cd9c
Zach Nielsen <zach@zachn.me>
2024-07-25 12:34:29 -0700

    add new tables

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use tesseract::Tesseract;
-use rusqlite::{Connection, Result};
+use rusqlite::{params, Connection, Result};
 use image::imageops::FilterType;
 use image::GenericImageView;
 use regex::Regex;
@@ -95,119 +95,145 @@ fn main() {
 }
 
 impl Itemizer{
-fn new() -> Self {
-    let s = Self {
-        db: Connection::open_in_memory().unwrap(),
-        items: load_items(),
-    };
+    fn new() -> Self {
+        let s = Self {
+            db: Connection::open_in_memory().unwrap(),
+            items: load_items(),
+        };
 
 
-    // Check if the 'person' table exists.
-    let table_exists: bool = s.db.query_row(
-        "SELECT EXISTS (SELECT 1 FROM sqlite_master WHERE type='table' AND name='items')",
-        [],
-        |row| row.get(0),
-    ).unwrap();
-
-    if table_exists {
-        println!("Loaded in memory DB");
-    } else {
-        println!("The 'items' table does not exist, creating it in-memory");
-        // TODO: tags:
-        // items table: remove price, now only a list of items (database copy of the items rules file)
-        // tags table (tag_id, tag_name)
-        // items_tags table (item_id, tag_id), links tags to items
-        // purchases table (id, item_id, price, date), this is the new 'running' table
-        s.db.execute(
-            "CREATE TABLE items (
-                    id       INTEGER PRIMARY KEY AUTOINCREMENT,
-                    code     INTEGER,
-                    name     TEXT NOT NULL,
-                    price    REAL NOT NULL
-                )",
-            [],
-        ).unwrap();
-    }
-
-    s
-}
-fn scan_files_in_dir(&mut self, dir: &Path) -> Result<(), Box<dyn std::error::Error>> {
-    for entry in std::fs::read_dir(dir).unwrap() {
-        let entry = entry.unwrap();
-        let entry_path = entry.path();
-        if entry.file_type().unwrap().is_dir() {
-            self.scan_files_in_dir(&entry_path)?;
-            continue;
-        }
-        let entry_str = entry_path.as_os_str().to_str().unwrap();
-        if image_done(entry_str) {
-            println!("image already done, skipping: {}", entry_str);
-            continue;
-        }
-
-        println!("About to open: {:?}", &entry_path);
-        let img = image::open(&entry_path).unwrap();
-        let (width, height) = img.dimensions();
-        println!("About to upscale: {:?}", &entry_path);
-        let upscale = 1.5;
-        let new_width = (width as f32 * upscale) as u32;
-        let new_height = (height as f32 * upscale) as u32;
-        // Resize the image using the Lanczos3 filter
-        let resized_img = image::imageops::resize(&img, new_width, new_height, FilterType::Lanczos3);
-        let resized_path =
-            [env::var("ITEMIZER_UPSCALED_IMAGE_DIR").expect("Env var not found: ITEMIZER_UPSCALED_IMAGE_DIR"),
-            entry.file_name().into_string().unwrap()]
-                .join("/");
-        println!("About to save upscaled to: {}", &resized_path);
-        resized_img.save(&resized_path).unwrap();
-        println!("Image has been upscaled and saved successfully.");
-
-        let tess = Tesseract::new(None, Some("eng")).unwrap();
-        let mut tess = tess.set_image(&resized_path).unwrap();
-        // let mut tess = tess.set_image(entry_str).unwrap();
-        let text = tess.get_text().unwrap();
-        println!("Recognized text:\n{}", text);
-        // TODO: Delete upscaled image
-
-
-        let receipt = Receipt::new(text);
-        for line in receipt.text.lines() {
-            let Some((code, desc, price)) = receipt.get_fields(line) else {
-                continue;
-            };
-            if self.items.codes.contains_key(&code) {
-                self.db.execute(
-                    "INSERT INTO items (code, name, price) VALUES (?1, ?2, ?3)",
-                    (
-                        &code,
-                        &self.items.codes[&code].name,
-                        &price,
-                    ),
-                )?;
-            } else if self.items.descr.contains_key(&desc) {
-                self.db.execute(
-                    "INSERT INTO items (code, name, price) VALUES (?1, ?2, ?3)",
-                    (
-                        &self.items.descr[&desc].code,
-                        &self.items.descr[&desc].name,
-                        &price,
-                    ),
-                )?;
-            } else {
-                // panic!("Could not find entry: {}", line);
-                println!("Could not find entry: {}", line);
-                // TODO: Ask for manual input, append to file
+        let tables = ["items", "tags", "item_tags", "purchases"];
+        for table in tables {
+            let table_exists: bool = s.db.query_row(
+                &format!("SELECT EXISTS (SELECT 1 FROM sqlite_master WHERE type='table' AND name='{}')", table),
+                [],
+                |row| row.get(0),
+            ).unwrap();
+            if !table_exists {
+                println!("The '{}' table does not exist, creating all tables", table);
+                s.create_tables();
             }
         }
 
-        let done_file = env::var("ITEMIZER_IMAGE_DONE_FILE").expect("Env var not found: ITEMIZER_IMAGE_DONE_FILE");
-        let mut done_fp = OpenOptions::new()
-            .append(true)
-            .open(done_file)?;
-        writeln!(done_fp, "{}", entry_str)?;
+        s
     }
-    Ok(())
-}
+
+    fn create_tables(&self) {
+        self.db.execute(
+            "CREATE TABLE items (
+                    item_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    code    INTEGER,
+                    name    TEXT NOT NULL,
+                )",
+            [],
+        ).unwrap();
+        self.db.execute(
+            "CREATE TABLE tags (
+                    tag_id   INTEGER PRIMARY KEY AUTOINCREMENT,
+                    tag_name TEXT NOT NULL UNIQUE
+                )",
+            [],
+        ).unwrap();
+        self.db.execute(
+            "CREATE TABLE items_tags (
+                    item_id INTEGER,
+                    tag_id  INTEGER,
+                    FOREIGN KEY (item_id) REFERENCES items(item_id),
+                    FOREIGN KEY (tag_id)  REFERENCES tags(tag_id),
+                    PRIMARY KEY (item_id, tag_id)
+                )",
+            [],
+        ).unwrap();
+        self.db.execute(
+            "CREATE TABLE purchases (
+                    purchase_id   INTEGER PRIMARY KEY AUTOINCREMENT,
+                    item_id       INTEGER,
+                    price         REAL,
+                    purchase_date TIMESTAMP,
+                    FOREIGN KEY (item_id) REFERENCES items(item_id)
+                )",
+            [],
+        ).unwrap();
+
+    }
+
+    fn scan_files_in_dir(&mut self, dir: &Path) -> Result<(), Box<dyn std::error::Error>> {
+        for entry in std::fs::read_dir(dir).unwrap() {
+            let entry = entry.unwrap();
+            let entry_path = entry.path();
+            if entry.file_type().unwrap().is_dir() {
+                self.scan_files_in_dir(&entry_path)?;
+                continue;
+            }
+            let entry_str = entry_path.as_os_str().to_str().unwrap();
+            if image_done(entry_str) {
+                println!("image already done, skipping: {}", entry_str);
+                continue;
+            }
+
+            println!("About to open: {:?}", &entry_path);
+            let img = image::open(&entry_path).unwrap();
+            let (width, height) = img.dimensions();
+            println!("About to upscale: {:?}", &entry_path);
+            let upscale = 1.5;
+            let new_width = (width as f32 * upscale) as u32;
+            let new_height = (height as f32 * upscale) as u32;
+            // Resize the image using the Lanczos3 filter
+            let resized_img = image::imageops::resize(&img, new_width, new_height, FilterType::Lanczos3);
+            let resized_path =
+                [env::var("ITEMIZER_UPSCALED_IMAGE_DIR").expect("Env var not found: ITEMIZER_UPSCALED_IMAGE_DIR"),
+                entry.file_name().into_string().unwrap()]
+                    .join("/");
+            println!("About to save upscaled to: {}", &resized_path);
+            resized_img.save(&resized_path).unwrap();
+            println!("Image has been upscaled and saved successfully.");
+
+            let tess = Tesseract::new(None, Some("eng")).unwrap();
+            let mut tess = tess.set_image(&resized_path).unwrap();
+            // let mut tess = tess.set_image(entry_str).unwrap();
+            let text = tess.get_text().unwrap();
+            println!("Recognized text:\n{}", text);
+            // TODO: Delete upscaled image
+
+
+            let receipt = Receipt::new(text);
+            for line in receipt.text.lines() {
+                let Some((code, desc, price)) = receipt.get_fields(line) else {
+                    continue;
+                };
+                if self.items.codes.contains_key(&code) {
+                    self.db.execute(
+                        "INSERT INTO items (code, name, price) VALUES (?1, ?2, ?3)",
+                        (
+                            &code,
+                            &self.items.codes[&code].name,
+                            &price,
+                        ),
+                    )?;
+                } else if self.items.descr.contains_key(&desc) {
+                    self.db.execute(
+                        "INSERT INTO items (code, name, price) VALUES (?1, ?2, ?3)",
+                        (
+                            &self.items.descr[&desc].code,
+                            &self.items.descr[&desc].name,
+                            &price,
+                        ),
+                    )?;
+                } else {
+                    // panic!("Could not find entry: {}", line);
+                    println!("Could not find entry: {}", line);
+                    // TODO: Ask for manual input, append to file
+                }
+            }
+
+            let done_file = env::var("ITEMIZER_IMAGE_DONE_FILE").expect("Env var not found: ITEMIZER_IMAGE_DONE_FILE");
+            let mut done_fp = OpenOptions::new()
+                .append(true)
+                .open(done_file)?;
+            writeln!(done_fp, "{}", entry_str)?;
+        }
+        Ok(())
+    }
 }
 
 fn image_done(image: &str) -> bool {
@@ -254,3 +280,8 @@ fn load_items() -> ItemDicts {
     ItemDicts {codes, descr}
 }
 
+fn name_is_in_table(conn: &Connection, table: &str, name: &str) -> bool {
+    let mut stmt = conn.prepare(&format!("SELECT COUNT(*) FROM {} WHERE name = ?", table)).unwrap();
+    let count: i64 = stmt.query_row(params![name], |row| row.get(0)).unwrap();
+    count > 0
+}
